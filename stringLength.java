1597b9d8aa0b5e160aedf3ff419f4c09e80d3a07
DevByAnu <anuradhakumawat2005@gmail.com>
2025-05-18 20:29:40 +0530

    To find out how many characters are in a string.
    This program finds and prints the length of a string using the length() method in Java.

---
+++ stringLength.java
@@ -0,0 +1,7 @@
+public class stringLength {
+    public static void main(String[] args) {
+        String txt="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The length of the txt string is:"+txt.length());
+    }
+    
+}
