d319758a81c7ab4b25ad9ed6175900a6cfd18f0e
DevByAnu <anuradhakumawat2005@gmail.com>
2025-05-18 20:22:35 +0530

    Widening Casting
    This program demonstrates widening casting in Java, where a smaller data type (int) is automatically converted to a larger data type (double) without data loss.

---
+++ wideningCasting.java
@@ -0,0 +1,10 @@
+public class wideningCasting {
+    public static void main(String[] args) {
+        int myInt=9;
+        double myDouble=myInt;
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+    
+}
