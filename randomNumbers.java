6f565750651e3103301c1c268cac39d085054bb6
DevByAnu <anuradhakumawat2005@gmail.com>
2025-05-18 20:24:41 +0530

    To generate random numbers for things like games.
    This program generates a random decimal number between 0.0 (inclusive) and 1.0 (exclusive) using Math.random() in Java.

---
+++ randomNumbers.java
@@ -0,0 +1,9 @@
+public class randomNumbers {
+    public static void main(String[] args) {
+       
+        //int randomNum=(int)(Math.random()*100);
+        System.out.println(Math.random());
+        
+    }
+    
+}
