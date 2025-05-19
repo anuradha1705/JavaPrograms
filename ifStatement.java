5cea312b738da30be1b64621e7a0620f9c6e69ab
DevByAnu <anuradhakumawat2005@gmail.com>
2025-05-19 21:26:47 +0530

    if statement
    Is program meh hum check karte hai ki variable x ki value y se badi hai ya nahi. Agar x ki value y se badi hoti hai, toh screen par message print hota hai "x is greater than y".

---
+++ ifStatement.java
@@ -0,0 +1,9 @@
+public class ifStatement {
+    public static void main(String[] args) {
+        int x=20, y=18;
+        if (x>y) {
+            System.out.println("x is greater then y");
+            
+        }
+    }
+}
