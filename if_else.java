b40c04ac996173c10e6a6884a00b7a627b7f7a63
DevByAnu <anuradhakumawat2005@gmail.com>
2025-05-19 21:28:39 +0530

    if-else Statement
    Is program meh hum check karte hai ki diya gaya number even hai ya odd.

---
+++ if_else.java
@@ -0,0 +1,11 @@
+public class if_else {
+    public static void main(String[] args) {
+        int num = 5;
+        if (num % 2 == 0) {
+            System.out.println("Even number");
+        }
+    else{
+        System.out.println("Odd number");
+    }
+    }
+}
