05f60bb82067d519dc870a33ad4a1a6fa69e7611
DevByAnu <anuradhakumawat2005@gmail.com>
2025-05-19 21:33:10 +0530

    Java else-if Statement Example
    This program demonstrates the use of the if-else if-else statement in Java. It checks whether a given number is positive, negative, or zero, and prints the result accordingly.

---
+++ else_if.java
@@ -0,0 +1,14 @@
+public class else_if{
+    public static void main(String[] args) {
+        int myNum = 10;
+        if(myNum>0){
+            System.out.println("The value is a positive number.");
+        }
+        else if (myNum<0) {
+            System.out.println("The value is a negative number.");
+        }else{
+            System.out.println("The value is 0.");
+        }
+    }
+    
+}
