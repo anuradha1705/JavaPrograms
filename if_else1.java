b83a7cd0892c742987e1e43fcf2b7d325ccb4518
DevByAnu <anuradhakumawat2005@gmail.com>
2025-05-19 21:34:22 +0530

    Java if-else Statement Example
    This program demonstrates the use of the if-else statement in Java to check whether a given number is even or odd. It uses the modulo (%) operator to determine if the number is divisible by 2. If the remainder is 0, it prints "even", otherwise it prints "odd".

---
+++ if_else1.java
@@ -0,0 +1,11 @@
+public class if_else1 {
+
+    public static void main(String[] args) {
+        int myNum=5;
+        if (myNum%2==0) {
+            System.out.println(myNum+" is even");
+        }else{
+            System.out.println(myNum+" is odd");
+        }
+    }
+}
